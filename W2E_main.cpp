aa3b4b76798ae820977d16d645a7460aa73e8c6f
Angel Sanchez <sanchezdiaz98@gmail.com>
2019-02-07 16:14:46 -0500

    Main cpp and list of cambridge locations

---
+++ W2E_main.cpp
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <time.h>
+
+using namespace std::
+
+void mEnu(){
+// Prints menu
+
+}
+
+int main()
+{
+	
+	return 0;
+}
+
+void rAnd(){
+
+//Generates random number
+
+	int RandomNumber;
+
+	srand(time(NULL)); //random seed
+	RandomNumber = rand() % 10 + 1;
+}
+
+void fIller(){
+//Fills in list from external
+}
